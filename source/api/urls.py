d1bb34bb02b4c25019cdd7eb18c9295368d19d8c
Nick <nicholasakho@gmail.com>
2025-08-03 01:22:09 +1200

    store user submission text

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import hello
+from .views import hello, search_view, submit_text_view, submissions_page
 from api.views import search_view
 
 urlpatterns = [
     path("hello/", hello),
     path('search/', search_view),
+    path('submit-text/', submit_text_view),
+    path('submissions/', submissions_page, name='submissions'),
 
 ]
--- source/api/views.py
+++ source/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from api.tepapa import search_tepapa
 
 @api_view(['GET'])
@@ -18,3 +19,22 @@ def search_view(request):
     except Exception as e:
         print(f"Te Papa API error: {e}")
         return Response({'error': 'Failed to fetch from Te Papa API'}, status=500)
+
+
+@api_view(['POST'])
+def submit_text_view(request):
+    user_text = request.data.get('text')
+    if not user_text:
+        return Response({'error': 'No text provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+    # Save to database
+    submission = UserSubmission.objects.create(text=user_text)
+
+    return Response({'message': 'Text received and saved', 'id': submission.id})
+
+from django.shortcuts import render
+from .models import UserSubmission
+
+def submissions_page(request):
+    all_submissions = UserSubmission.objects.order_by('-submitted_at')
+    return render(request, 'submissions.html', {'submissions': all_submissions})
