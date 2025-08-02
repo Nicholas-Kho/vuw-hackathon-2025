c57e65a09c2c956af675942af2f1e84f5b3558a7
nickkho <nicholasakho@gmail.com>
2025-08-02 16:08:23 +1200

    added components

--- source/frontend/src/App.js
+++ source/frontend/src/App.js
@@ -1,5 +1,14 @@
 import { useEffect, useState } from 'react';
 
+import Silk from './React Components/Silk';
+import "./App.css"
+import Iridescence from './React Components/Iridescence';
+import BlurText from "./React Components/BlurText";
+
+const handleAnimationComplete = () => {
+  console.log('Animation completed!');
+};
+
 function App() {
   const [message, setMessage] = useState('');
   const [results, setResults] = useState([]);
@@ -22,11 +31,44 @@ function App() {
     }, []);
 
   return (
-  <div>
+  <div class="container">
+    <div className="container1">
+      <nav className="sidebar">
+        <h2>Menu</h2>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+      <div className="main-content">
+        <h1>
+        <BlurText
+  text="Isn't this so cool?!"
+  delay={150}
+  animateBy="words"
+  direction="top"
+  onAnimationComplete={handleAnimationComplete}
+  className="text-2xl mb-8"
+/></h1>
+
+
+        <p>This is the main content area.</p>
+      </div>
+    </div>
+
+    <div class="silk_background">
+      <Silk
+      speed={5}
+      scale={1}
+      color="#7B7481"
+      noiseIntensity={1.5}
+      rotation={0}
+    />
+    </div>
+    <div class="contents">
   <p>yo</p>
     <h1>{message}</h1>         {/* from /api/hello/ */}
 <pre>{JSON.stringify(results, null, 2)}</pre>  {/* from /api/search/ */}
-
+</div>
     </div>
   );
 }
