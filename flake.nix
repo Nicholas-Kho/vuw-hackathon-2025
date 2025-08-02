d7a9a54fb533c3c9e897e049da4167c65aa98711
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2025-08-02 12:43:52 +1200

    add nix flake to contain devtools

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Tools for working on thing";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ python313Packages.pip ];
+          shellHook = ''
+            echo "Entering Python development shell..."
+            # Create and activate venv if it doesn't exist
+            if [ ! -d ".venv" ]; then
+              python3 -m venv .venv
+            fi
+            source .venv/bin/activate
+          '';
+        };
+      });
+}
+
