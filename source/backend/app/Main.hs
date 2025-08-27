e0cc210b20e86d717ca981c19799a6fd587894a1
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2025-08-27 13:27:54 +1200

    add quick function to test API endpoint as well as dependencies

--- source/backend/app/Main.hs
+++ source/backend/app/Main.hs
@@ -5,11 +5,18 @@ module Main (main) where
 import Config (Config (..), Stage (..), mkConfig, pickLogger)
 import Configuration.Dotenv as DE
 import Control.Monad.Logger (runStdoutLoggingT)
+import Data.Text (Text, pack)
 import Database.Persist.Sql (runSqlPool)
 import Models (doMigrations)
-import Network.Wai.Handler.Warp (Port)
+import Network.Wai.Handler.Warp (Port, defaultSettings)
 import System.Environment (lookupEnv)
 import Text.Read (readMaybe)
+
+import ApiTepapa (agentById)
+import Network.HTTP.Client (newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Servant
+import Servant.Client (BaseUrl (..), Scheme (..), mkClientEnv, runClientM)
 
 main :: IO ()
 main = do
@@ -17,9 +24,26 @@ main = do
     port <- getPort
     stage <- decideStage
     config <- mkConfig stage
+    key <- expectKey <$> lookupEnv "API_KEY"
     let loggerMiddleware = pickLogger stage
         pool = getPool config
     runStdoutLoggingT $ runSqlPool doMigrations pool
+    apiTest key
+
+apiTest :: String -> IO ()
+apiTest key = do
+    manager <- newManager tlsManagerSettings
+    res <- runClientM (agentById (Just (pack key)) 6594) (mkClientEnv manager baseUrl)
+    print res
+
+baseUrl :: BaseUrl
+baseUrl =
+    BaseUrl
+        { baseUrlScheme = Https
+        , baseUrlHost = "data.tepapa.govt.nz"
+        , baseUrlPort = 443
+        , baseUrlPath = "collection/"
+        }
 
 getPort :: IO Port
 getPort = do
@@ -35,3 +59,7 @@ ensure predicate val = if predicate val then pure val else fail ""
 withDefault :: Maybe a -> a -> a
 withDefault Nothing fallback = fallback
 withDefault (Just x) _ = x
+
+expectKey :: Maybe String -> String
+expectKey (Just k) = k
+expectKey Nothing = error "You need an API key to be able to call the Te Papa API. Make sure there is an API_KEY=... variable in your .env file."
