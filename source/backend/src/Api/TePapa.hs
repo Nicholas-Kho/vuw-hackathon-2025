55feca6aaaaf12c0730dfa46bc3d93e703d03839
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-01-11 22:10:44 +1300

    Add quick API draft

--- source/backend/src/Api/TePapa.hs
+++ source/backend/src/Api/TePapa.hs
@@ -1,1 +1,12 @@
-module Api.TePapa () where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Api.TePapa where
+
+import Data.Text
+import Servant.API
+import TePapa.Decode
+
+type NeedsKey api = Header' '[Required] "x-api-key" Text :> api
+
+type TePapaApi = NeedsKey ("object" :> Capture "id" Int :> Get '[JSON] RawItem)
