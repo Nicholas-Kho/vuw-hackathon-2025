437ee2dba0437d54dcfc570136a89276832257a7
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-01-18 12:31:33 +1300

    Add some more helper methods to CommonObject

--- source/backend/src/TePapa/CommonObject.hs
+++ source/backend/src/TePapa/CommonObject.hs
@@ -6,8 +6,10 @@ module TePapa.CommonObject (
     fromCategory,
     fromObjectResponse,
     fromPlace,
+    fromRelated,
     fromTopic,
     getCommon,
+    getId,
 ) where
 
 import TePapa.Decode
@@ -51,3 +53,34 @@ fromCategory = ACategory
 
 fromTopic :: Topic -> TePapaThing
 fromTopic = ATopic
+
+fromRelated :: RelatedThings -> [TePapaThing]
+fromRelated
+    RelatedThings
+        { relatedSpecimens = rs
+        , relatedPlaces = rps
+        , relatedPeople = rpl
+        , relatedOrgs = ro
+        , relatedArtefacts = ra
+        } =
+        concat
+            [ AnArtefact <$> ra
+            , ASpecimen <$> rs
+            , APerson <$> rpl
+            , AnOrg <$> ro
+            , APlace <$> rps
+            ]
+
+getId :: TePapaThing -> TePapaReference
+getId thing =
+    let
+        thingEid = (getCommon thing).eid
+     in
+        case thing of
+            APerson _ -> TePapaReference{namespace = AgentR, eid = thingEid}
+            AnOrg _ -> TePapaReference{namespace = AgentR, eid = thingEid}
+            AnArtefact _ -> TePapaReference{namespace = ObjectR, eid = thingEid}
+            ASpecimen _ -> TePapaReference{namespace = ObjectR, eid = thingEid}
+            APlace _ -> TePapaReference{namespace = PlaceR, eid = thingEid}
+            ACategory _ -> TePapaReference{namespace = ConceptR, eid = thingEid}
+            ATopic _ -> TePapaReference{namespace = TopicR, eid = thingEid}
