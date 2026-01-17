fae67ef1028dd5332001132eea33176402059a08
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-01-17 19:37:54 +1300

    Add common object type

---
+++ source/backend/src/TePapa/CommonObject.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+
+module TePapa.CommonObject (
+    TePapaThing (..),
+    fromAgentResponse,
+    fromCategory,
+    fromObjectResponse,
+    fromPlace,
+    getCommon,
+) where
+
+import TePapa.Decode
+
+data TePapaThing
+    = APerson !Person
+    | AnOrg !Organization
+    | AnArtefact !Artefact
+    | ASpecimen !Specimen
+    | APlace !Place
+    | ACategory !Category
+
+getCommon :: TePapaThing -> CommonFields
+getCommon t = case t of
+    APerson p -> p.com
+    AnOrg o -> o.com
+    AnArtefact a -> a.com
+    ASpecimen s -> s.com
+    APlace p -> p.com
+    ACategory c -> c.com
+
+fromObjectResponse :: ObjectResponse -> TePapaThing
+fromObjectResponse resp =
+    case resp of
+        Art a -> AnArtefact a
+        Spc s -> ASpecimen s
+
+fromAgentResponse :: AgentResponse -> TePapaThing
+fromAgentResponse resp =
+    case resp of
+        Prs p -> APerson p
+        Org g -> AnOrg g
+
+fromPlace :: Place -> TePapaThing
+fromPlace = APlace
+
+fromCategory :: Category -> TePapaThing
+fromCategory = ACategory
