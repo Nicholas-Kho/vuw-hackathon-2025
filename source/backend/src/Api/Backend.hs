431d9540948ab656e417262be6d2a1c0915679a8
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-01-19 18:12:15 +1300

    make servant spec for game facing API

--- source/backend/src/Api/Backend.hs
+++ source/backend/src/Api/Backend.hs
@@ -1,1 +1,35 @@
-module Api.Backend () where
+{-# LANGUAGE DeriveAnyClass #-}
+
+module Api.Backend (
+    BackendApi,
+    ClientGraphAction (..),
+    InitialGameState (..),
+) where
+
+import Data.Aeson
+import Domain.Model
+import GHC.Generics
+import Servant.API
+import TePapa.Convert (GraphAction)
+
+type BackendApi =
+    "start" :> Get '[JSON] InitialGameState
+        :<|> "expand" :> ReqBody '[JSON] ExpandParams :> Post '[JSON] [GraphAction]
+
+data ClientGraphAction
+    = AddNode NodeId NodeContent
+    | AddEdge Edge
+    deriving (Show, Generic, ToJSON)
+
+data InitialGameState = InitialGameState
+    { startAt :: NodeId
+    , endAt :: NodeId
+    , subgraph :: [ClientGraphAction]
+    }
+    deriving (Generic, ToJSON)
+
+data ExpandParams = ExpandParams
+    { expandAboutId :: NodeId
+    , iAlreadyHave :: [NodeId]
+    }
+    deriving (Generic, FromJSON)
