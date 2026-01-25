94c8e96a06f568e65a5250970de1308224bed9e5
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-01-25 13:17:50 +1300

    bring in Elm stuff

--- flake.nix
+++ flake.nix
@@ -10,12 +10,14 @@
       let
         pkgs = import nixpkgs { inherit system; };
         ghcVersion = "ghc9102";
+        elmPkgs = pkgs.elmPackages;
         haskellPkgs = pkgs.haskell.packages.${ghcVersion}.override {
           overrides = self: super: {
             system-fileio = pkgs.haskell.lib.dontCheck super.system-fileio;
           };
         };
         muselinks = haskellPkgs.callCabal2nix "muselinks" ./. { };
+        # muselinks-api-codegen = haskellPkgs.callCabal2nix "api-codegen" ./. { };
       in {
         packages.default = muselinks;
         devShells.default = haskellPkgs.shellFor {
@@ -23,11 +25,13 @@
           nativeBuildInputs = with pkgs; [
             haskellPkgs.haskell-language-server
             haskellPkgs.fourmolu
-            # haskellPkgs.hlint
             haskellPkgs.stan
-            sqlite
             cabal-install
             ghciwatch
+            elmPkgs.elm
+            elmPkgs.elm-analyse
+            elmPkgs.elm-format
+            elmPkgs.elm-language-server
           ];
         };
       });
