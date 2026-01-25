1b5b145e9cfcd4ea54a7ed9c3b6a1aac795a0c3b
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-01-26 09:32:05 +1300

    write the build pipeline in nix

--- flake.nix
+++ flake.nix
@@ -3,12 +3,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:numtide/flake-utils";
+    mkElmDerivation.url = "github:jeslie0/mkElmDerivation";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, mkElmDerivation, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ mkElmDerivation.overlays.default ];
+        };
         ghcVersion = "ghc9102";
         elmPkgs = pkgs.elmPackages;
         haskellPkgs = pkgs.haskell.packages.${ghcVersion}.override {
@@ -16,12 +20,35 @@
             system-fileio = pkgs.haskell.lib.dontCheck super.system-fileio;
           };
         };
-        muselinks = haskellPkgs.callCabal2nix "muselinks" ./. { };
-        # muselinks-api-codegen = haskellPkgs.callCabal2nix "api-codegen" ./. { };
+        muselinks-server = haskellPkgs.callCabal2nix "muselinks" ./. { };
+        muselinks-api-codegen = haskellPkgs.callCabal2nix "api-codegen" ./. { };
+        muselinks-frontend = pkgs.mkElmDerivation {
+          name = "muselinks-frontend";
+          src = ./frontend;
+          elmJson = ./frontend/elm.json;
+          nativeBuildInputs = [ elmPkgs.elm muselinks-api-codegen ];
+          buildPhase = ''
+            mkdir -p src/Generated
+            ${muselinks-api-codegen}/bin/api-codegen src/Generated
+            elm make Main.elm --output=index.html
+          '';
+          installPhase = ''
+            mkdir -p $out/
+            cp index.html $out/
+          '';
+        };
       in {
-        packages.default = muselinks;
+        packages.default = pkgs.symlinkJoin {
+          name = "muselinks";
+          paths = [ muselinks-server muselinks-frontend ];
+          postBuild = ''
+            mkdir -p $out/static
+            ln -s ${muselinks-frontend}/index.html $out/static/index.html
+            ln -s ${muselinks-frontend}/assets $out/static/assets
+          '';
+        };
         devShells.default = haskellPkgs.shellFor {
-          packages = p: [ muselinks ];
+          packages = p: [ muselinks-server muselinks-api-codegen ];
           nativeBuildInputs = with pkgs; [
             haskellPkgs.haskell-language-server
             haskellPkgs.fourmolu
