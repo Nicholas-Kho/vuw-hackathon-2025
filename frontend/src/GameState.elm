6e01716abc4c88eb18d25c6bffca8824f2aeea50
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-01-27 16:47:56 +1300

    update gamestate

--- frontend/src/GameState.elm
+++ frontend/src/GameState.elm
@@ -1,8 +1,7 @@
 module GameState exposing (..)
 
+import Camera exposing (Camera, mkCamera)
 import Generated.BackendApi exposing (InitialGameState, NodeId, Subgraph)
-import Math.Vector2 as W
-import Math.Vector3 as WV
 
 
 type alias GameState =
@@ -10,39 +9,6 @@ type alias GameState =
     , endAt : NodeId
     , graph : Subgraph
     , cam : Camera
-    }
-
-
-type alias Camera =
-    { pos : W.Vec Float
-    , basis : Mat2
-    }
-
-
-type alias Mat2 =
-    { row1 : W.Vec Float
-    , row2 : W.Vec Float
-    }
-
-
-type alias Mat3 =
-    { row1 : WV.Vec Float
-    , row2 : WV.Vec Float
-    , row3 : WV.Vec Float
-    }
-
-
-mid2 : Mat2
-mid2 =
-    { row1 = W.vec2 1 0
-    , row2 = W.vec2 0 1
-    }
-
-
-mkCamera : Camera
-mkCamera =
-    { pos = W.vec2 0 0
-    , basis = mid2
     }
 
 
