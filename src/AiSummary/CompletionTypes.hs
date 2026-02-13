25495c4623201e70248a6de070a295b0c5728635
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-02-13 13:10:11 +1300

    write completion params encoder

--- src/AiSummary/CompletionTypes.hs
+++ src/AiSummary/CompletionTypes.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module AiSummary.CompletionTypes (
     CompletionParams (..),
     CompletionResponse (..),
 ) where
 
+import Data.Aeson
+import Data.Aeson.Encoding (text)
 import Data.Text
+import GHC.Generics
 
 data CompletionParams = CompletionParams
     { model :: Text
@@ -12,4 +19,32 @@ data CompletionParams = CompletionParams
     , maxTokens :: Int
     , temperature :: Float
     }
+    deriving (Generic)
+
+data Role = System | User deriving (Generic)
+
+instance ToJSON Role where
+    toEncoding r =
+        case r of
+            System -> text "system"
+            User -> text "user"
+
+data Message = Message
+    { role :: Role
+    , content :: Text
+    }
+    deriving (Generic, ToJSON)
+
+instance ToJSON CompletionParams where
+    toEncoding params =
+        pairs
+            ( "model" .= params.model
+                <> "maxTokens" .= params.maxTokens
+                <> "temperature" .= params.temperature
+                <> "messages"
+                    .= [ Message System params.systemPrompt
+                       , Message User params.userPrompt
+                       ]
+            )
+
 type CompletionResponse = ()
