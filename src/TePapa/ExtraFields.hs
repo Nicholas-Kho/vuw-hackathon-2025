1decdf02a64ba5b1ed612a3bb365489e3988097d
city-high-rider <60808209+city-high-rider@users.noreply.github.com>
2026-02-19 16:16:56 +1300

    add function to take extra fields to field map

--- src/TePapa/ExtraFields.hs
+++ src/TePapa/ExtraFields.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module TePapa.ExtraFields (
@@ -10,9 +11,12 @@ module TePapa.ExtraFields (
     ExtrasSpecimen (..),
     ExtrasTopic (..),
     Geolocation (..),
+    toExtrasMap,
 ) where
 
 import Data.Aeson
+import qualified Data.Map.Strict as M
+import Data.Maybe (catMaybes)
 import Data.Text
 
 data Extras
@@ -55,6 +59,8 @@ data Geolocation = Geolocation
     { lat :: Float
     , lon :: Float
     }
+instance Show Geolocation where
+    show gl = (Prelude.show gl.lat) <> "lat, " <> (Prelude.show gl.lon) <> "lon"
 
 data ExtrasTopic = ExtrasTopic
     { narrative :: Text
@@ -69,3 +75,56 @@ instance FromJSON Geolocation where
                     <$> o .: "lat"
                     <*> o .: "lon"
             )
+
+class ToExtrasMap a where
+    toExtrasMap :: a -> M.Map Text Text
+
+instance ToExtrasMap ExtrasSpecimen where
+    toExtrasMap e =
+        M.fromList
+            [ ("collectionLabel", e.collectionLabel)
+            , ("captionFormatted", captionFormatted e)
+            ]
+
+instance ToExtrasMap ExtrasArtefact where
+    toExtrasMap e =
+        M.fromList
+            [ ("collectionLabel", e.collectionLabel)
+            , ("caption", caption e)
+            ]
+
+instance ToExtrasMap ExtrasPerson where
+    toExtrasMap e =
+        M.fromList
+            [ ("gender", e.gender)
+            , ("familyName", e.familyName)
+            , ("givenName", e.givenName)
+            , ("verbatimBirthDate", e.verbatimBirthDate)
+            , ("verbatimDeathDate", e.verbatimDeathDate)
+            ]
+
+instance ToExtrasMap ExtrasOrganisation where
+    toExtrasMap e =
+        M.fromList
+            [ ("verbatimBirthDate", e.verbatimBirthDate)
+            , ("verbatimDeathDate", e.verbatimDeathDate)
+            ]
+
+instance ToExtrasMap ExtrasPlace where
+    toExtrasMap e =
+        M.fromList $
+            catMaybes
+                [ if Prelude.null (nations e) then Nothing else Just ("nations", Data.Text.intercalate ", " e.nations)
+                , fmap (\loc -> ("location", Data.Text.show loc)) e.location
+                ]
+
+instance ToExtrasMap ExtrasTopic where
+    toExtrasMap e = M.singleton "narrative" e.narrative
+
+instance ToExtrasMap Extras where
+    toExtrasMap (ForPerson p) = toExtrasMap p
+    toExtrasMap (ForOrganisation o) = toExtrasMap o
+    toExtrasMap (ForArtefact a) = toExtrasMap a
+    toExtrasMap (ForSpecimen s) = toExtrasMap s
+    toExtrasMap (ForPlace p) = toExtrasMap p
+    toExtrasMap (ForTopic t) = toExtrasMap t
